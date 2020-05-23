c80e1ba780dc7fbb5c67dd113acc3adfdcf91246
Manuel Maqueda <manumaqueda@gmail.com>
2020-05-23 16:50:02 +0000

    adding main functionality

--- serve/predict.py
+++ serve/predict.py
@@ -69,9 +69,10 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    input_data_words = review_to_words(input_data)
+    input_data_words_converted = convert_and_pad(model.word_dict, input_data_words)
+    data_X = input_data_words_converted[0]
+    data_len = input_data_words_converted[1]
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -86,7 +87,7 @@ def predict_fn(input_data, model):
 
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
-
     result = None
-
-    return result
+    with torch.no_grad():
+        output = model.forward(data)
+    result = np.round(output.numpy())
